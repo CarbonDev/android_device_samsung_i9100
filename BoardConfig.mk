e364ae43f82c5264c511d16d02bc6d8be369cc6d
Myself5 <thchod@gmail.com>
2014-06-08 12:26:15 +0200

    Enable Swipe in CWM
    Change-Id: I6b3369523a7c25496ad8df0017a62cd82f96b477

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,9 @@ TARGET_KERNEL_CONFIG := custom_i9100_defconfig
 # Rom/Kernel Toolchain Sabermod 4.8/4.7
 TARGET_GCC_VERSION_EXP := 4.8
 
+# Swipe CWM
+BOARD_RECOVERY_SWIPE := true
+
 # assert
 TARGET_OTA_ASSERT_DEVICE := galaxys2,i9100,GT-I9100,GT-I9100M,GT-I9100P,GT-I9100T,SC-02C
 
